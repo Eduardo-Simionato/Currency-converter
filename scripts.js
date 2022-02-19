ee62a9e7bad2269dfbfadda019cde71ceade73ac
Eduardo Simionato <eduardogssg@hotmail.com>
2022-02-19 10:40:50 -0300

    Firts commit

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
+
+const dolar = 5.2
+const euro = 5.8
+
+const convertValues = () => {
+  const inputReal = document.getElementById("input-real").value
+  const realValueText = document.getElementById("real-value-text")
+  const currencyValueText = document.getElementById("currency-value-text")
+
+  realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputReal)
+
+  if (select.value === "US$ Dólar americano") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputReal / dolar)
+  }
+
+  if (select.value === "€ Euro") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputReal / euro)
+  }
+ 
+
+}
+
+changeCurrency = () => {
+  const currencyName = document.getElementById("currency-name")
+  const currencyImg = document.getElementById("currency-img")
+
+  if (select.value === "€ Euro") {
+    currencyName.innerHTML = "Euro"
+    currencyImg.src = "./assets/Euro.svg"
+  }
+
+  if (select.value === "US$ Dólar americano") {
+    currencyName.innerHTML = "Dólar americano"
+    currencyImg.src = "./assets/estados-unidos (1) 1.svg"
+  }
+
+  if (select.value === "BTC") {
+    currencyName.innerHTML = "BTC"
+    currencyImg.src = "./assets/Bitcoin.svg"
+  }
+
+  convertValues()
+}
+
+button.addEventListener("click", convertValues)
+select.addEventListener("change", changeCurrency)
